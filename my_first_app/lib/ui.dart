bf427f667ea7c0aedcb308cd632b3a5f658d65d6
koraish3438 <koraishmdkajamall38@gmail.com>
2026-08-04 11:22:33 +0600

    Develop

--- my_first_app/lib/ui.dart
+++ my_first_app/lib/ui.dart
@@ -1,14 +1,2 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("This is a ui for my app"),
-            child: Text("Hello class"),
-        ),
-      ),
-    ),
-  );
-}
