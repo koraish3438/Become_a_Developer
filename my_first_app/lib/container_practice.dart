1f22d40f4b98e80107872bac0df929e5455213f9
koraish3438 <koraishmdkajamall38@gmail.com>
2026-08-05 12:00:01 +0600

    develop

--- my_first_app/lib/container_practice.dart
+++ my_first_app/lib/container_practice.dart
@@ -10,13 +10,27 @@ class ContainerPractice extends StatelessWidget {
         title: Center(child: Text("Container", style: TextStyle(color: Colors.white),)),
         backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: Container(
-          width:100,
-          height:100,
-          color: Colors.blue,
-          child: Center(child: Text("This is me", style: TextStyle(color: Colors.white),)),
-        ),
+      body: Row(
+        children: [
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.red,
+            child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.amber,
+            child: Center(child: Text("Blue", style: TextStyle(color: Colors.black),)),
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.black,
+            child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
+          ),
+        ],
       ),
     );
   }
