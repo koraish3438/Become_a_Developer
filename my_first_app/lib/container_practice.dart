3d45150cdcce2736b397e04a39910be34f96a397
koraish3438 <koraishmdkajamall38@gmail.com>
2026-08-05 12:16:31 +0600

    develop

--- my_first_app/lib/container_practice.dart
+++ my_first_app/lib/container_practice.dart
@@ -10,48 +10,51 @@ class ContainerPractice extends StatelessWidget {
         title: Center(child: Text("Container", style: TextStyle(color: Colors.white),)),
         backgroundColor: Colors.blue,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.red,
-                child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.amber,
-                child: Center(child: Text("Blue", style: TextStyle(color: Colors.black),)),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.black,
-                child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
-              ),
-            ],
-          ),
-          
-          // Work by column
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.black,
-            child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
-          ),
-          Container(
-            width: 100,
-            height: 100,
-            color: Colors.amber,
-            child: Center(child: Text("Black", style: TextStyle(color: Colors.black),)),
-          ),
-          
-        ],
-      ),
+      body: Align(
+        alignment: Alignment.topLeft, // Same work a crossAxisAlignment
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                  child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(child: Text("Blue", style: TextStyle(color: Colors.black),)),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black,
+                  child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
+                ),
+              ],
+            ),
+
+            // Work by column
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.black,
+              child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.amber,
+              child: Center(child: Text("Black", style: TextStyle(color: Colors.black),)),
+            ),
+
+          ],
+        ),
+      )
     );
   }
 }
