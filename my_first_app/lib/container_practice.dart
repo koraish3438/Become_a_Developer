8c8bd804c77d524e0cd63bce24163ee23ba536fa
koraish3438 <koraishmdkajamall38@gmail.com>
2026-08-10 10:41:54 +0600

    scrollview

--- my_first_app/lib/container_practice.dart
+++ my_first_app/lib/container_practice.dart
@@ -16,39 +16,72 @@ class ContainerPractice extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
-                    child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
-                    child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
-                    child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.amber,
-                    child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black),)),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.black,
-                    child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
-                  ),
-                ],
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.red,
+                      child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.red,
+                      child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.red,
+                      child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.amber,
+                      child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black),)),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.black,
+                      child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.red,
+                      child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.red,
+                      child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.red,
+                      child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.amber,
+                      child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black),)),
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.black,
+                      child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
+                    ),
+                  ],
+                ),
               ),
           
               // Work by column
