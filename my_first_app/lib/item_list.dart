2a7bcf5948d64d225fb3bc195ee3329dd6e65c1f
koraish3438 <koraishmdkajamall38@gmail.com>
2026-08-11 11:24:08 +0600

    list builder

--- my_first_app/lib/item_list.dart
+++ my_first_app/lib/item_list.dart
@@ -2,47 +2,26 @@ import 'package:flutter/material.dart';
 
 class ItemList extends StatelessWidget {
   const ItemList({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
+
+    var arrNames = ["Amdadul", "Ashik", "Koraish", "Jui", "Rifat", "Kajam", "Jannati"];
+
     return Scaffold(
         appBar: AppBar(
           title: Center(child: Text("Item List", style: TextStyle(color: Colors.white),)),
           backgroundColor: Colors.blue,
         ),
-        body: ListView(
+        body: ListView.builder(itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(arrNames[index], style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),),
+          );
+        },
+          itemCount: arrNames.length,
+          itemExtent: 150,
           scrollDirection: Axis.horizontal,
-          reverse: true,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("One", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Two", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Three", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Four", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Five", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Six", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("Seven", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),),
-            ),
-          ],
         )
     );
   }
