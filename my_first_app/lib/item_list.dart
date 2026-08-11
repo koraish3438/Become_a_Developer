fe959dd14917e224b7536c4eb73c62fd2a898dae
koraish3438 <koraishmdkajamall38@gmail.com>
2026-08-11 11:29:48 +0600

    list separator

--- my_first_app/lib/item_list.dart
+++ my_first_app/lib/item_list.dart
@@ -13,15 +13,27 @@ class ItemList extends StatelessWidget {
           title: Center(child: Text("Item List", style: TextStyle(color: Colors.white),)),
           backgroundColor: Colors.blue,
         ),
-        body: ListView.builder(itemBuilder: (context, index) {
+        body:
+        // ListView.builder(itemBuilder: (context, index) {
+        //   return Padding(
+        //     padding: const EdgeInsets.all(8.0),
+        //     child: Text(arrNames[index], style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),),
+        //   );
+        // },
+        //   itemCount: arrNames.length,
+        //   itemExtent: 150,
+        //   scrollDirection: Axis.horizontal,
+        // )
+        ListView.separated(itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.all(8.0),
             child: Text(arrNames[index], style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),),
           );
         },
           itemCount: arrNames.length,
-          itemExtent: 150,
-          scrollDirection: Axis.horizontal,
+          separatorBuilder: (context, index) {
+            return Divider(height: 50, thickness: 2,);
+          },
         )
     );
   }
