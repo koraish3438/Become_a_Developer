0c448adcd3a4c24f6edb3cfc10afa3b4741a9bb4
koraish3438 <koraishmdkajamall38@gmail.com>
2026-08-11 11:14:55 +0600

    list

--- my_first_app/lib/item_list.dart
+++ my_first_app/lib/item_list.dart
@@ -11,6 +11,8 @@ class ItemList extends StatelessWidget {
           backgroundColor: Colors.blue,
         ),
         body: ListView(
+          scrollDirection: Axis.horizontal,
+          reverse: true,
           children: [
             Padding(
               padding: const EdgeInsets.all(8.0),
