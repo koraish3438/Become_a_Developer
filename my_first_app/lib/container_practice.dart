ea01f764c73f5b3c630edc9ceb5770339d223789
koraish3438 <koraishmdkajamall38@gmail.com>
2026-08-12 10:25:08 +0600

    decoration of container

--- my_first_app/lib/container_practice.dart
+++ my_first_app/lib/container_practice.dart
@@ -13,178 +13,202 @@ class ContainerPractice extends StatelessWidget {
       body: Align(
         alignment: Alignment.topLeft, // Same work a crossAxisAlignment
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.red,
-                      child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.red,
-                      child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.red,
-                      child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.amber,
-                      child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black),)),
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.black,
-                      child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.red,
-                      child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.red,
-                      child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.red,
-                      child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.amber,
-                      child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black),)),
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.black,
-                      child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
-                    ),
-                  ],
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: 100,
+                          height: 100,
+                          decoration: BoxDecoration(
+                            color: Colors.lightBlueAccent,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(21),
+                              bottomRight: Radius.circular(21),
+                            ),
+                            border: Border.all(
+                              width: 2,
+                              color: Colors.black,
+                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 11,
+                                spreadRadius: 2,
+                                color: Colors.black
+                              ),
+                            ],
+                          ),
+                          child: Center(child:
+                          Text("Me", style: TextStyle(color: Colors.white),)),
+                        ),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.red,
+                        child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.black12,
+                        child: Center(child: Text("Black", style: TextStyle(color: Colors.black),)),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.amber,
+                        child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black),)),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.black,
+                        child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.red,
+                        child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.red,
+                        child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.red,
+                        child: Center(child: Text("Red", style: TextStyle(color: Colors.white),)),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.amber,
+                        child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black),)),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 100,
+                        color: Colors.black,
+                        child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-          
-              // Work by column
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.black,
-                child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.amber,
-                child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black, fontSize: 25),)),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.black,
-                child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.amber,
-                child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black, fontSize: 25),)),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.black,
-                child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.amber,
-                child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black, fontSize: 25),)),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.black,
-                child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.amber,
-                child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black, fontSize: 25),)),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.black,
-                child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.amber,
-                child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black, fontSize: 25),)),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.black,
-                child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.amber,
-                child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black, fontSize: 25),)),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.black,
-                child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.amber,
-                child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black, fontSize: 25),)),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.black,
-                child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.amber,
-                child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black, fontSize: 25),)),
-              ),
-              ElevatedButton(onPressed: () {
-                Navigator.pop(context);
-              }, child: Text("Back to Home"))
-            ],
+            
+                // Work by column
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black,
+                  child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black, fontSize: 25),)),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black,
+                  child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black, fontSize: 25),)),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black,
+                  child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black, fontSize: 25),)),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black,
+                  child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black, fontSize: 25),)),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black,
+                  child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black, fontSize: 25),)),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black,
+                  child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black, fontSize: 25),)),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black,
+                  child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black, fontSize: 25),)),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.black,
+                  child: Center(child: Text("Black", style: TextStyle(color: Colors.white),)),
+                ),
+                Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(child: Text("Yellow", style: TextStyle(color: Colors.black, fontSize: 25),)),
+                ),
+                ElevatedButton(onPressed: () {
+                  Navigator.pop(context);
+                }, child: Text("Back to Home"))
+              ],
+            ),
           ),
         ),
       )
