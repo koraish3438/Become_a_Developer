4bbc9bdb676058bfad6106250a42d6c55865928a
koraish3438 <koraishmdkajamall38@gmail.com>
2026-08-31 09:38:58 +0600

    grid view

--- my_first_app/lib/my_grid_view.dart
+++ my_first_app/lib/my_grid_view.dart
@@ -12,45 +12,79 @@ class MyGridView extends StatelessWidget {
           child: Text("Grid View", style: TextStyle(color: Colors.white),),
         ),
       ),
-      body: GridView.count(crossAxisCount: 3,
-        // crossAxisSpacing: 11,
-        // mainAxisSpacing: 11,
+      body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(color: Colors.green,),
+          Container(
+            height: 200,
+            child: GridView.count(crossAxisCount: 5,
+              
+              // crossAxisSpacing: 11,
+              // mainAxisSpacing: 11,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(color: Colors.green,),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(color: Colors.grey,),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(color: Colors.blue,),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(color: Colors.black,),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(color: Colors.orange,),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(color: Colors.pinkAccent,),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(color: Colors.amber,),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(color: Colors.yellow,),
+                ),
+              ],
+            ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(color: Colors.grey,),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(color: Colors.blue,),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(color: Colors.black,),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(color: Colors.orange,),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(color: Colors.pinkAccent,),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(color: Colors.amber,),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(color: Colors.yellow,),
+          
+          Container(
+            height: 100,
           ),
 
+          Container(
+            height: 300,
+            child: GridView.extent(maxCrossAxisExtent: 100,
+              crossAxisSpacing: 11,
+              mainAxisSpacing: 11,
+              children: [
+                Container(color: Colors.green,),
+                Container(color: Colors.orange,),
+                Container(color: Colors.pink,),
+                Container(color: Colors.amber,),
+                Container(color: Colors.black,),
+                Container(color: Colors.blue,),
+                Container(color: Colors.grey,),
+                Container(color: Colors.lightBlueAccent,),
+                Container(color: Colors.yellow,),
+                Container(color: Colors.deepOrange,),
+                Container(color: Colors.red,),
+                Container(color: Colors.brown,),
+              ],
+            ),
+          )
         ],
       ),
+      
     );
   }
 }
